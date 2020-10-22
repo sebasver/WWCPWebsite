a82cdd3960f60081e386ffdef1a337be66f4202c
Sébastiaan Verheyen <sebastiaan.verheyen@student.kdg.be>
2020-10-22 23:07:16 +0200

    Loco list rough draft implemented

--- js/content/Plist.js
+++ js/content/Plist.js
@@ -1,1 +1,27 @@
 addEventListener("load", init, false);
+
+function init() {
+
+    let PassengerList = document.getElementById("PassengerList");
+    PassengerList.innerHTML = PassengerList.innerHTML +
+        '<table>' +
+        '<tr><th></th><th>Name</th><th># Skins</th><th># Seats</th></tr>'+
+        '<tr><td><a href=""><img src="" alt=""></a></td><td>4th Generation Dosto</td><td>Skins#</td><td>Seats#</td></tr>' +
+        '<tr><td><a href=""><img src="" alt=""></a></td><td>Eurofima Open 1</td><td>Skins#</td><td>Seats#</td></tr>' +
+        '<tr><td><a href=""><img src="" alt=""></a></td><td>Eurofima Open 1-2</td><td>Skins#</td><td>Seats#</td></tr>' +
+        '<tr><td><a href=""><img src="" alt=""></a></td><td>Eurofima Open 2</td><td>Skins#</td><td>Seats#</td></tr>' +
+        '<tr><td><a href=""><img src="" alt=""></a></td><td>Eurofima Compartment 1</td><td>Skins#</td><td>Seats#</td></tr>' +
+        '<tr><td><a href=""><img src="" alt=""></a></td><td>Eurofima Compartment 1-2</td><td>Skins#</td><td>Seats#</td></tr>' +
+        '<tr><td><a href=""><img src="" alt=""></a></td><td>Eurofima Compartment 2</td><td>Skins#</td><td>Seats#</td></tr>' +
+        '<tr><td><a href=""><img src="" alt=""></a></td><td>Eurofima Couchette</td><td>Skins#</td><td>Seats#</td></tr>' +
+        '<tr><td><a href=""><img src="" alt=""></a></td><td>SA4u28</td><td>Skins#</td><td>Seats#</td></tr>' +
+        '<tr><td><a href=""><img src="" alt=""></a></td><td>SA4u28 Christmas</td><td>Skins#</td><td>Seats#</td></tr>' +
+        '<tr><td><a href=""><img src="" alt=""></a></td><td>SA4uk28</td><td>Skins#</td><td>Seats#</td></tr>' +
+        '<tr><td><a href=""><img src="" alt=""></a></td><td>SA4uk28 Christmas</td><td>Skins#</td><td>Seats#</td></tr>' +
+        '<tr><td><a href=""><img src="" alt=""></a></td><td>SB4u28</td><td>Skins#</td><td>Seats#</td></tr>' +
+        '<tr><td><a href=""><img src="" alt=""></a></td><td>SB4u28 Christmas</td><td>Skins#</td><td>Seats#</td></tr>' +
+        '<tr><td><a href=""><img src="" alt=""></a></td><td>SB4uk28</td><td>Skins#</td><td>Seats#</td></tr>' +
+        '<tr><td><a href=""><img src="" alt=""></a></td><td>SB4uk28 Christmas</td><td>Skins#</td><td>Seats#</td></tr>' +
+        '<tr><td><a href=""><img src="" alt=""></a></td><td>SPw4u28</td><td>Skins#</td><td>Seats#</td></tr>' +
+        '</table>';
+}
